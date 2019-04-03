b96a040ea388e170d2d0c7eb3e1731e80370110c
cody.yi <xu.yi@gongbangbang.com>
2019-04-03 17:57:33 +0800

    demo添加

---
+++ app/src/main/java/com/cody/live/event/bus/app/AppDemo.java
@@ -0,0 +1,14 @@
+package com.cody.live.event.bus.app;
+
+import com.cody.live.event.bus.lib.annotation.Event;
+import com.cody.live.event.bus.lib.annotation.EventScope;
+
+/**
+ * Created by xu.yi. on 2019/4/3.
+ * LiveEventBus
+ */
+@EventScope(name = "demo",active = true)
+public enum AppDemo {
+    @Event(description = "定义一个测试事件",data = String.class)testString,
+    @Event(description = "定义一个测试事件测试对象",data = TestBean.class)testBean,
+}
